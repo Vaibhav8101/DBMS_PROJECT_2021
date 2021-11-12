d567da3187c9453feebd390896ed5dddd65be68e
vaibhav yadav <vaibhav.yadav8101@gmail.com>
2021-11-12 23:31:00 +0530

    update-record-vaibhav

--- routes/main.js
+++ routes/main.js
@@ -10,7 +10,7 @@ const bodyParser = require("body-parser");//for reading form data
 const mysqlConnection = require("../connection");
 const encoder = bodyParser.urlencoded();
 express().use(express.static(path.join(__dirname, "../public")));
-var sessionUsername,category,searchValue;
+var sessionUsername, category, searchValue;
 
 
 //signup request
@@ -62,6 +62,7 @@ router1.post("/login", encoder, async function (req, res) {
         if (await bcrypt.compare(password, results[0].password)) {
             console.log("Login successful");
             req.session.Username = username;
+            req.session.password = password;
             sessionUsername = req.session.Username;
             // console.log(req.session.Username);
             res.redirect("/service");
@@ -126,8 +127,8 @@ router1.get("/login", function (req, res) {
 
 
 //search bar
-router1.post("/search/:category", encoder, async function (req, res){
-    searchValue=req.body.search;
+router1.post("/search/:category", encoder, async function (req, res) {
+    searchValue = req.body.search;
     category = req.params.category;
     console.log(category);
     console.log(searchValue);
@@ -135,12 +136,12 @@ router1.post("/search/:category", encoder, async function (req, res){
 })
 router1.get("/search", function (req, res) {
     // res.sendFile(path.join(__dirname, "../public/html/login.html"))
-    mysqlConnection.query("Select * from books where category = ? and title like ?",[category,'%'+searchValue+'%'], (err, rows, fields) => {
+    mysqlConnection.query("Select * from books where category = ? and title like ?", [category, '%' + searchValue + '%'], (err, rows, fields) => {
         if (!err) {
             rows['category'] = 'R';
             rows['Name'] = "Rent"
-            var cG=rows['category'];
-            res.render("books", { rows: rows,category:cG, layout: 'ListBook.handlebars' })
+            var cG = rows['category'];
+            res.render("books", { rows: rows, category: cG, layout: 'ListBook.handlebars' })
         } else {
             // res.send(err);
             console.log(err);
@@ -148,6 +149,176 @@ router1.get("/search", function (req, res) {
         }
     })
 })
+router1.get("/updateRecord", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/updateRecord.html"))
+})
 
+//update records code start from here
+router1.post("/upRoll", encoder, async function (req, res) {
+    var rollno = req.body.rollno;
+    console.log(req.session.Username);
+    connection.query("update student set roll_no=? where username = ? ", [rollno, req.session.Username], async function (error, results, fields) {
+        if (error) {
+            res.redirect("/unSuccess");
+            throw error;
+        }
+        else {
+            res.redirect("/uSuccess");
+        }
+        res.end();
+    })
+})
+
+
+router1.post("/upfname", encoder, async function (req, res) {
+    var fname = req.body.fname;
+    console.log(req.session.Username);
+    connection.query("update student set fname=? where username = ? ", [fname, req.session.Username], async function (error, results, fields) {
+        if (error) {
+            res.redirect("/unSuccess");
+            throw error;
+        }
+        else {
+            res.redirect("/uSuccess");
+        }
+        res.end();
+    })
+})
+
+
+router1.post("/uplname", encoder, async function (req, res) {
+    var lname = req.body.lname;
+    console.log(req.session.Username);
+    connection.query("update student set lname=? where username = ? ", [lname, req.session.Username], async function (error, results, fields) {
+        if (error) {
+            res.redirect("/unSuccess");
+            throw error;
+        }
+        else {
+            res.redirect("/uSuccess");
+        }
+        res.end();
+    })
+})
+
+router1.post("/upBranch", encoder, async function (req, res) {
+    var branch = req.body.branch;
+    console.log(req.session.Username);
+    connection.query("update student set branch=? where username = ? ", [branch, req.session.Username], async function (error, results, fields) {
+        if (error) {
+            res.redirect("/unSuccess");
+            throw error;
+        }
+        else {
+            res.redirect("/uSuccess");
+        }
+        res.end();
+    })
+})
+
+router1.post("/upEmail", encoder, async function (req, res) {
+    var email = req.body.email;
+    console.log(req.session.Username);
+    connection.query("update student set email=? where username = ? ", [email, req.session.Username], async function (error, results, fields) {
+        if (error) {
+            res.redirect("/unSuccess");
+            throw error;
+        }
+        else {
+            res.redirect("/uSuccess");
+        }
+        res.end();
+    })
+})
+
+router1.post("/upPhone", encoder, async function (req, res) {
+    var phoneNo = req.body.phoneNo;
+    console.log(req.session.Username);
+    connection.query("update student set phone_no=? where username = ? ", [phoneNo, req.session.Username], async function (error, results, fields) {
+        if (error) {
+            res.redirect("/unSuccess");
+            throw error;
+        }
+        else {
+            res.redirect("/uSuccess");
+        }
+        res.end();
+    })
+})
+
+router1.post("/upGen", encoder, async function (req, res) {
+    var gender = req.body.gender;
+    console.log(req.session.Username);
+    connection.query("update student set gender=? where username = ? ", [gender, req.session.Username], async function (error, results, fields) {
+        if (error) {
+            res.redirect("/unSuccess");
+            throw error;
+        }
+        else {
+            res.redirect("/uSuccess");
+        }
+        res.end();
+    })
+})
+
+router1.post("/upYear", encoder, async function (req, res) {
+    var year = req.body.year;
+    console.log(req.session.Username);
+    connection.query("update student set year=? where username = ? ", [year, req.session.Username], async function (error, results, fields) {
+        if (error) {
+            res.redirect("/unSuccess");
+            throw error;
+        }
+        else {
+            res.redirect("/uSuccess");
+        }
+        res.end();
+    })
+})
+
+router1.post("/upSem", encoder, async function (req, res) {
+    var sem = req.body.sem;
+    console.log(req.session.Username);
+    connection.query("update student set sem=? where username = ? ", [sem, req.session.Username], async function (error, results, fields) {
+        if (error) {
+            res.redirect("/unSuccess");
+            throw error;
+        }
+        else {
+            res.redirect("/uSuccess");
+        }
+        res.end();
+    })
+})
+
+router1.post("/upPass", encoder, async function (req, res) {
+    var prev_password = req.body.prev_password;
+    var new_password = req.body.new_password;
+    console.log(req.session.Username);
+    console.log(req.session.password);
+    if (req.session.password == prev_password) {
+        const hashpsw = await bcrypt.hash(new_password, 6);
+        connection.query("update student set password=? where username = ? ", [hashpsw, req.session.Username], async function (error, results, fields) {
+            if (error) {
+                res.redirect("/unSuccess");
+                throw error;
+            }
+            else {
+                res.redirect("/uSuccess");
+            }
+            res.end();
+        })
+    }
+})
+
+
+
+
+router1.get("/uSuccess", function (req, res) {
+    res.render("updateSuccess");
+})
+router1.get("/unSuccess", function (req, res) {
+    res.render("updateunsuccess");
+})
 module.exports = router1;
 
